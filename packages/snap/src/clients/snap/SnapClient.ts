0612d083d5d25b39dab59a3dbd205a571a2e89f3
Ulisses Ferreira <ulisses@hey.com>
2025-09-05 12:07:07 +0100

    chore: move snap methods to SnapClient

--- packages/snap/src/clients/snap/SnapClient.ts
+++ packages/snap/src/clients/snap/SnapClient.ts
@@ -8,12 +8,44 @@ import type {
 } from '@metamask/snaps-sdk';
 
 import type { Preferences } from '../../types/snap';
+import { EntropySourceId } from '@metamask/keyring-api';
+import { JsonSLIP10Node } from '@metamask/key-tree';
 
 /**
  * Client for interacting with the Snap API.
  * Provides methods for managing interfaces, dialogs, preferences, and background events.
  */
 export class SnapClient {
+  /**
+   * Retrieves a `SLIP10NodeInterface` object for the specified path and curve.
+   *
+   * @param params - The parameters for the Solana key derivation.
+   * @param params.entropySource - The entropy source to use for key derivation.
+   * @param params.path - The BIP32 derivation path for which to retrieve a `SLIP10NodeInterface`.
+   * @param params.curve - The elliptic curve to use for key derivation.
+   * @returns A Promise that resolves to a `SLIP10NodeInterface` object.
+   */
+  async getBip32Entropy({
+    entropySource,
+    path,
+    curve,
+  }: {
+    entropySource?: EntropySourceId | undefined;
+    path: string[];
+    curve: 'secp256k1' | 'ed25519';
+  }): Promise<JsonSLIP10Node> {
+    const node = await snap.request({
+      method: 'snap_getBip32Entropy',
+      params: {
+        path,
+        curve,
+        ...(entropySource ? { source: entropySource } : {}),
+      },
+    });
+  
+    return node;
+  }
+
   /**
    * Gets the state of an interactive interface by its ID.
    *
