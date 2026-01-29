3f91daf0a3635a2e17ee0c58e0cb8c4661b8ad35
Michele Esposito <michele@esposito.codes>
2026-01-29 21:44:11 +0100

    fix: include missing cases

--- packages/snap/src/clients/security-alerts-api/utils.ts
+++ packages/snap/src/clients/security-alerts-api/utils.ts
@@ -1,6 +1,5 @@
 import { add0x } from '@metamask/utils';
-import { TronWeb } from 'tronweb';
-import type { Transaction } from 'tronweb/lib/esm/types';
+import { TronWeb, type Types } from 'tronweb';
 
 import type { SecurityScanPayload } from './types';
 
@@ -13,30 +12,30 @@ import type { SecurityScanPayload } from './types';
  * @returns The extracted scan parameters.
  */
 export const extractScanParametersFromTransactionData = (
-  rawData: Transaction['raw_data'],
+  rawData: Types.Transaction['raw_data'],
 ): SecurityScanPayload | null => {
   const contractParam = rawData.contract[0]?.parameter.value;
   if (!contractParam) {
     return null;
   }
 
-  let to = '';
-  let value = 0;
-  let data: string | null = null;
   const from = TronWeb.address.fromHex(contractParam.owner_address);
 
-  if ('contract_address' in contractParam) {
+  let to = '';
+  if ('contract_address' in contractParam && contractParam.contract_address) {
     to = TronWeb.address.fromHex(contractParam.contract_address);
-    if ('call_value' in contractParam && contractParam.call_value) {
-      value = contractParam.call_value;
-    }
+  } else if ('to_address' in contractParam && contractParam.to_address) {
+    to = TronWeb.address.fromHex(contractParam.to_address);
   }
 
-  if ('to_address' in contractParam) {
-    to = TronWeb.address.fromHex(contractParam.to_address);
+  let value = 0;
+  if ('call_value' in contractParam && contractParam.call_value) {
+    value = contractParam.call_value;
+  } else if ('amount' in contractParam && contractParam.amount) {
     value = contractParam.amount;
   }
 
+  let data: string | null = null;
   if ('data' in contractParam && contractParam.data) {
     data = add0x(contractParam.data);
   }
