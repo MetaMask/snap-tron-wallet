7bf9d3ef331aabd4d678347d30e44d640ff41769
Michele Esposito <michele@esposito.codes>
2026-02-10 14:06:45 +0100

    test: enable coverage collection and thresholds

--- packages/snap/jest.config.js
+++ packages/snap/jest.config.js
@@ -3,6 +3,34 @@
  * @type {import('ts-jest').JestConfigWithTsJest}
  */
 const config = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['./src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: ['.*/index\\.ts'],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'babel',
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ['text', 'html', 'json-summary', 'lcov'],
+
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    global: {
+      branches: 50.25,
+      functions: 51.16,
+      lines: 63.82,
+      statements: 63.88,
+    },
+  },
+
   preset: '@metamask/snaps-jest',
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest',
