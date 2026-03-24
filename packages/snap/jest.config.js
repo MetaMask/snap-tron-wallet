ecdaa57f3ff85dc7090da74e22909f060d7abb29
Michele Esposito <michele@esposito.codes>
2026-03-24 18:04:59 +0100

    test: adjust coverage thresholds

--- packages/snap/jest.config.js
+++ packages/snap/jest.config.js
@@ -24,10 +24,10 @@ const config = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 55.79,
-      functions: 62.52,
-      lines: 71.49,
-      statements: 71.55,
+      branches: 55.8,
+      functions: 62.45,
+      lines: 71.4,
+      statements: 71.46,
     },
   },
 
