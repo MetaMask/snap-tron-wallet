52edff3606bce8e2ab4980e4a04fc47c0ce88cd8
Ulisses <ulisses@hey.com>
2026-03-30 15:38:49 +0100

    test: update coverage thresholds

--- packages/snap/jest.config.js
+++ packages/snap/jest.config.js
@@ -24,10 +24,10 @@ const config = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 55.79,
+      branches: 55.92,
       functions: 62.52,
-      lines: 71.53,
-      statements: 71.58,
+      lines: 71.55,
+      statements: 71.61,
     },
   },
 
